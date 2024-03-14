9c5c068f3b98f119dc18a8fba73807f7f537c9b4
Arijit Paine <99323868+codewithap@users.noreply.github.com>
2024-03-14 20:16:38 +0530

    Add files via upload

--- scrappers/gogoanime/gogo.py
+++ scrappers/gogoanime/gogo.py
@@ -1,99 +1,74 @@
 from bs4 import BeautifulSoup
 import  requests, json
+import time
 
 
 class gogo:
   def __init__(self) -> None:
-    self.BASE_AJAX_URL = "https://ajax.gogo-load.com"
+    self.BASE_URL = "https://ww3.gogoanimes.fi"
+    self.BASE_AJAX_URL = "https://ajax.gogocdn.net"
     # self.BASE_URL = urls.gogo1
   
   def search(self,query):
     data = requests.get(f"{self.BASE_AJAX_URL}/site/loadAjaxSearch?keyword={query}")
-    # soup = BeautifulSoup(json.loads(data.text)["content"],"html.parser")
-    return data
+    soup = BeautifulSoup(json.loads(data.text)["content"], "html.parser")
+    json_data = [
+        {
+            "title": title.text,
+            "thumbnail": thumbnail["style"][17:-2],
+            "id": gogoId["href"].replace("category/", ""),
+        }
+        for title, thumbnail, gogoId in zip(
+            soup.select(".ss-title"),
+            soup.select(".ss-title div"),
+            soup.select(".ss-title"),
+        )
+    ]
+    return json_data
     
 
-  def anime(self, gogoid):
-    data = requests.get(f"{self.BASE_URL}/category/{gogoid}")
-    soup = BeautifulSoup(data.text, "html.parser")
-    id = soup.select("#movie_id")[0]["value"]
-    name = soup.select("#alias_anime")[0]["value"]
-    episData = requests.get(f"{self.BASE_AJAX_URL}/ajax/load-list-episode?ep_start=0&ep_end=9999&id={id}&alias={name}")
-    episSoup = BeautifulSoup(episData.content, 'html.parser')
+  def get_anime_info(self, gogoid):
+    base_url = self.BASE_URL
+    data = requests.get(f"{base_url}/category/{gogoid}")
+    html = BeautifulSoup(data.text, "html.parser")
+
+    anime_id = html.select("#movie_id")[0]["value"]
+    anime_name = html.select("#alias_anime")[0]["value"]
+
+    epis_data = requests.get(f"{self.BASE_AJAX_URL}/ajax/load-list-episode?ep_start=0&ep_end=9999&id={anime_id}&alias={anime_name}")
+    epis = BeautifulSoup(epis_data.content, 'html.parser')
+    eparr = [x["href"].replace(" /", "") for x in epis.select("a")][::-1]
+
+    genre_links = html.select(".anime_info_body .type")[2].select("a")
+    genre = [{"title": a["title"], "url": f"genre/{a['href'].split('/genre/')[1]}"} for a in genre_links]
+
+    status_link = html.select(".anime_info_body .type")[4].select("a")[0]
+    status = {"title": status_link["title"], "url": status_link["href"]}
+
+    info = {
+        "title": html.select(".anime_info_body h1")[0].text,
+        "thumbnail": html.select(".anime_info_body img")[0]["src"],
+        "type": html.select(".anime_info_body .type")[0].text.split("\n")[1],
+        "description": html.select(".anime_info_body .type")[1].text.replace("Plot Summary: ", ""),
+        "genre": genre,
+        "released": html.select(".anime_info_body .type")[3].text.replace("Released: ", ""),
+        "status": status,
+        "other_name": html.select(".anime_info_body .type")[5].text.replace("Other name: ", ""),
+        "episodes": eparr
+    }
+    return info
     
   
-  def episode(self,gogoEpId):
+  def get_episode_servers(self,gogoEpId):
     data = requests.get(f"{self.BASE_URL}/{gogoEpId}")
     soup = BeautifulSoup(data.text,"html.parser")
+    servers = [{"video": x["data-video"],"name": x.text.replace("Choose this server","").replace("\n","")} for x in soup.select(".anime_muti_link ul li a")]
+    return json.dumps(servers, indent = 2)
     
 
 
 if __name__ == "__main__":
-    gogo().search("naruto")
+    t = time.time()
+    print(gogo().get_episode_servers("naruto-episode-205"))
+    print(time.time() - t)
 
-
-
-"""
-
-class gogoJsonGenerator:
-  def __init__(self) -> None:
-    pass
-  def searchJson(soup): 
-    titles = [x.text for x in soup.select(".ss-title")]
-    thumbnail = [(x["style"])[17:-2] for x in soup.select(".ss-title div")]
-    gogoId =[x["href"] for x in soup.select(".ss-title")]
-    json_data = "["
-    for i in range(len(titles)):
-      json_data += '{"title":"'+titles[i]+'","thumbnail":"'+thumbnail[i]+'","id": "'+gogoId[i].replace("category/","")+'"},'
-    return json.loads(json_data[:-1]+"]")
-  
-  def animeJson(html,epis):
-    eparr = [x["href"].replace(" /","") for x in epis.select("a")][::-1]
-    genre = html.select(".anime_info_body .type")[2].select("a")
-    status = html.select(".anime_info_body .type")[4]
-    info = {
-      "title": html.select(".anime_info_body h1")[0].text,
-      "thumbnail": html.select(".anime_info_body img")[0]["src"],
-      "type": html.select(".anime_info_body .type")[0].text.split("\n")[1],
-      "description": html.select(".anime_info_body .type")[1].text.replace("Plot Summary: ",""),
-      "genre": [{"title":a["title"], "url": "genre/"+a["href"].split("/genre/")[1]} for a in genre],
-      "released" : html.select(".anime_info_body .type")[3].text.replace("Released: ",""),
-      "status" : {"title": status.select("a")[0]["title"],"url" : status.select("a")[0]["href"]},
-      "other_name" : html.select(".anime_info_body .type")[5].text.replace("Other name: ",""),
-      "episodes": eparr
-    }
-    return json.dumps(info)
-  def episodeJson(soup):
-    servers = [{"video": x["data-video"],"name": x.text.replace("Choose this server","").replace("\n","")} for x in soup.select(".anime_muti_link ul li a")]
-    return json.dumps(servers, indent = 2)
-
-## myanimelist.net ##
-class malJsonGenerator:
-  def __init__(self) -> None:
-    pass
-
-  def searchAnime(data,page):
-    soup = BeautifulSoup(data.text, "html.parser")
-    last_page = 20 # int(soup.select(".ac .spaceit .bgColor1 a")[-1].text)
-    animes = soup.select("#content div.list table tr")
-    del animes[0]
-    dataDict = {
-      "pagination":{
-        "next_page": True if page < last_page else False,
-        "prev_page": True if page > 1 else False,
-        "page": page
-      },
-      "items": [
-        {
-          "title": anime.select(".title a")[0].text,
-          "mal_id": int(anime.select(".picSurround a")[0]["href"].replace("https://myanimelist.net/anime/","").split("/")[0]),
-          "type": anime.select(".ac")[0].text.strip(" \n"),
-          "url": anime.select(".picSurround a")[0]["href"],
-          "img": (anime.select(".picSurround img")[0]["data-srcset"].split(" 1x, ")[1].replace(" 2x","")),
-          "score": (anime.select(".ac")[2].text.strip(" \n"))
-        } for anime in animes
-      ]
-    }
-    return json.dumps(dataDict)
-
-"""
