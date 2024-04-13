c140225c787a56e3373e69c9c7bfa8abd857a0a5
Arijit Paine <99323868+codewithap@users.noreply.github.com>
2024-04-13 18:11:11 +0530

    Add files via upload

--- scrappers/myanimelist.py
+++ scrappers/myanimelist.py
@@ -19,13 +19,22 @@ class mal:
     songs = []
     for x in songsarr:
        if len(x) != 0 and x[0]["value"] != "":
-        songs.append(x[0]["value"].split("/track/")[1])
-    jsonData = {
+        songs.append(x[0]["value"]) #.split("/track/")[1])
+    jsonData = { 
       "mal_id" : int(id),
       "title": soup.select(".title-name")[0].text,
       "title_eng": soup.select(".title-english")[0].text,
       "url" : f"{self.BaseUrl}/anime/{id}",
-      "img" : soup.select(".borderClass img.ac")[0]["data-src"],
+      "imgs": {
+            "jpg":{
+              "medium": soup.select(".borderClass img.ac")[0]["data-src"],
+              "large": soup.select(".borderClass img.ac")[0]["data-src"].replace(".jpg", "l.jpg")
+            },
+            "webp": {
+              "medium": soup.select(".borderClass img.ac")[0]["data-src"].replace(".jpg", ".webp"),
+              "large": soup.select(".borderClass img.ac")[0]["data-src"].replace(".jpg", "l.webp")
+            }
+          },
       "rank": soup.select(".stats-block .ranked strong")[0].text,
       "popularity": soup.select(".stats-block .popularity strong")[0].text,
       "score": soup.select(".stats-block .score")[0].text,
@@ -79,7 +88,17 @@ class mal:
           "mal_id": int(anime.select(".picSurround a")[0]["href"].replace("https://myanimelist.net/anime/","").split("/")[0]),
           "type": anime.select(".ac")[0].text.strip(" \n"),
           "url": anime.select(".picSurround a")[0]["href"],
-          "imgs": {"small": (anime.select(".picSurround img")[0]["data-srcset"].split(" 1x, ")[1].replace(" 2x","")),"large": "".join((anime.select(".picSurround img")[0]["data-srcset"].split(" 1x, ")[1].replace(" 2x","")).split("r/100x140/")).split("?s=")[0]},
+          "imgs": {
+            "jpg":{
+              "small": (anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x",""),
+              "medium": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0],
+              "large": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0].replace(".jpg", "l.jpg")
+            },
+            "webp": {
+              "medium": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0].replace(".jpg", ".webp"),
+              "large": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0].replace(".jpg", "l.webp")
+            }
+          },
           "score": (anime.select(".ac")[2].text.strip(" \n"))
         } for anime in animes]}
     return json.dumps(dataDict)
@@ -121,7 +140,17 @@ class mal:
           "mal_id": anime.select("a")[0]["href"].replace("https://myanimelist.net/","").split("/")[1],
           "type": anime.select("a")[0]["href"].replace("https://myanimelist.net/","").split("/")[0],
           "url": anime.select("a")[0]["href"],
-          "imgs": {"small": (anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x",""),"large": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0]},
+          "imgs": {
+            "jpg":{
+              "small": (anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x",""),
+              "medium": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0],
+              "large": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0].replace(".jpg", "l.jpg")
+            },
+            "webp": {
+              "medium": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0].replace(".jpg", ".webp"),
+              "large": "".join((anime.select("img")[0]["data-srcset"].split(", ")[1]).replace(" 2x","").split("r/100x140/")).split("?s=")[0].replace(".jpg", "l.webp")
+            }
+          },
           "rank": anime.select(".top-anime-rank-text")[0].text,
           "score": anime.select("td.score.ac.fs14 > div > span")[0].text,
           "otherInfo": anime.select(".information")[0].text.strip()
